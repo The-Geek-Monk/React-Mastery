18f993e214211266ce9f45b385de203dcab466da
TheGeekMonk <vasusehgal007@gmail.com>
2022-08-30 09:59:20 +0530

    converting html website to react

---
+++ two-website/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Vasu Sehgal</h1>
+      <Navigation />
+    </div>
+  );
+};
+export default App;
